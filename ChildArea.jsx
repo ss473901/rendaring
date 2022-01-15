b5b9cc62426ff0b9eef4e23d517065f40451816a
眞田悟司 <84009331+ss473901@users.noreply.github.com>
2022-01-16 03:18:34 +0900

    remove paragraph

--- ChildArea.jsx
+++ ChildArea.jsx
@@ -16,7 +16,7 @@ export const ChildArea = (props) => {
     <>
       {open ? (
         <div style={style}>
-          <p>子コンポーネント</p>
+          <p>コンポーネント</p>
         </div>
       ) : null}
     </>
